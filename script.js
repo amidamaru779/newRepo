42c8461051c85e51cc55e4f13646f05c68c53a5d
Nikita Polev <nikitko779@gmail.com>
2024-08-15 19:21:52 +0300

    усложнённое задание

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const isNumber = function(num){
     return(!isNaN(num) && isFinite(num))
 }
 
-const startGame = function (botNumber) {
+const startGame = function (botNumber, attempt) {
     title = confirm("Угадай число от 1 до 100")
     if (title === false){
         alert("Спасибо за уделённое время. Досвидание!");
@@ -14,28 +14,36 @@ const startGame = function (botNumber) {
     }
     
     const play = function () {
-        number = prompt("Пожалуйста введите число")
+        if (0 < attempt){
+            number = prompt("Пожалуйста введите число")
         
-        if (number === null){
-            alert("Игра окончена!");
-            return
-        }
-
-        if (!isNumber(number)){
-            alert("Введите число!");
-            play();
-        }
+            if (number === null){
+                alert("Игра окончена! Спасибо за игру!");
+                return
+            }
         
-        if (number > botNumber){
-            alert("Загаданное число меньше");
-            play()
-        }else if (number < botNumber){
-            alert("Загаданное число больше");
-            play()   
-        } 
 
-        if (+number === botNumber){
-            alert("Поздравляю, Вы угадали!");
+            if (!isNumber(number)){
+                alert("Введите число!");
+                play();
+            }
+        
+            if (number > botNumber){
+                attempt--
+                alert("Загаданное число меньше.Осталось" + " " + attempt + " " + "попыток!");
+                play()
+            }else if (number < botNumber){
+                attempt--
+                alert("Загаданное число больше.Осталось" + " " + attempt + " " + "попыток!");
+                play()   
+            } 
+
+            if (+number === botNumber){
+                alert("Поздравляю, Вы угадали!!! Хотели бы сыграть ещё ?");
+                startGame(botNumber = Math.floor(Math.random( ) * (100 - 1 + 1)) + 1, 10);
+            }
+        } else {
+            alert("Попытки закончились, хотите сыграть еще?")
         }
         
         
@@ -43,4 +51,4 @@ const startGame = function (botNumber) {
     play()
 }
 
-startGame(Math.floor(Math.random( ) * (100 - 1 + 1)) + 1)
+startGame(Math.floor(Math.random( ) * (100 - 1 + 1)) + 1, 10);
