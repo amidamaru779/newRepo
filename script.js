9d28e5e30237e73e7065ed8352d30b6041f8250e
Nikita Polev <nikitko779@gmail.com>
2024-09-11 18:16:28 +0300

    выполнил урок 16

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-function DomElement(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.createElem();
+'use strict'
 
-}
-DomElement.prototype.createElem = function () {
-    let elem;
-    if (this.selector.startsWith('.')) {
-        elem = document.createElement('div');
-        elem.classList.add(this.selector.slice(1));
-    } else if (this.selector.startsWith('#')) {
-        elem = document.createElement('p');
-        elem.id = this.selector.slice(1);
+class First {
+    sayHello() {
+        console.log('Привет я метод родителя!');
     }
+}
 
-    elem.style.height = this.height;
-    elem.style.width = this.width;
-    elem.style.backgroundColor = this.bg;
-    elem.style.fontSize = this.fontSize;
-    elem.textContent = 'Hello, world!';
-    document.body.append(elem);
-};
-const newElement = new DomElement('.block', '100px', '200px', 'red', '12px')
-const newElement1 = new DomElement('#block', '100px', '200px', 'blue', '20px')
+class Second extends First {
+    sayHello() {
+        super.sayHello()
+        console.log('А я наследуемый метод!');
+    }
+}
+const objFirst = new First()
+objFirst.sayHello()
+
+const objSecond = new Second()
+objSecond.sayHello()
